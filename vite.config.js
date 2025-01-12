085fd196f630c749eb6e732a3a0a8f9a9b26f8f7
TayMcQuaya <taymcquaya@gmail.com>
2025-01-13 07:52:04 +0800

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Computer-Hardware-Test/', // Replace with your repository name
+  base: '/Computer-Hardware-Test/'
 })
